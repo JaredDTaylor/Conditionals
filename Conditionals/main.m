058e8455fde9bf6853592b4be66548da4df92ee4
Jared <jaredtaylor@cbrc.net>
2015-03-25 11:48:44 -0400

    added else statements

--- Conditionals/main.m
+++ Conditionals/main.m
@@ -11,7 +11,29 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        float bankAccount = 2000000000.0;
+        if (bankAccount < 0){
+            NSLog(@"You have a negative balance");
+            
+        }
+        else if (bankAccount >= 50000 && bankAccount < 1000000){
+            NSLog(@"You are rich!!!");
+            
+            
+        }
+        else if (bankAccount >= 1000000  && bankAccount < 1000000000){
+            NSLog(@"You should donate money");
+            
+        }
+        else if (bankAccount >= 1000000000){
+            NSLog(@"You suck!");
+            
+        }
+        else {
+            NSLog(@"You have money!");
+            
+            
+        }
     }
     return 0;
 }
